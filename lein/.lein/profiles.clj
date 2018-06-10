cedf536c11bc6f9fde3aa56a6d1cf98ff34e719d
Mike Richards <mrichards42@gmx.com>
2018-06-09 23:22:05 -0400

    Add lein profiles.clj

---
+++ lein/.lein/profiles.clj
@@ -0,0 +1,6 @@
+{:user {:plugins [[cider/cider-nrepl "0.17.0"]
+                  [lein-kibit "0.1.5"]
+                  [lein-pprint "1.2.0"]]
+        :dependencies [[cljfmt "0.5.7"]
+                       [jonase/kibit "0.1.5"
+                        :exclusions [org.clojure/clojure]]]}}
