a84681daccd301a1c87adb1108b5dd823abccee8
Mike Richards <mrichards42@gmx.com>
2018-10-18 10:35:11 -0400

    Add a few lein plugins/deps
    * lein-ancient -- for out of date deps
    * ultra -- much improved repl
    * pomegranate -- on-the-fly dependencies

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,6 +1,9 @@
 {:user {:plugins [[cider/cider-nrepl "0.17.0"]
                   [lein-kibit "0.1.5"]
-                  [lein-pprint "1.2.0"]]
+                  [lein-pprint "1.2.0"]
+                  [lein-ancient "0.6.15"]
+                  [venantius/ultra "0.5.2"]]
         :dependencies [[cljfmt "0.5.7"]
                        [jonase/kibit "0.1.5"
-                        :exclusions [org.clojure/clojure]]]}}
+                        :exclusions [org.clojure/clojure]]
+                       [com.cemerick/pomegranate "1.0.0"]]}}
