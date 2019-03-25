0fb06dc6f74e8095a80d95dd656cb911a0e1b645
Mike Richards <mrichards42@gmx.com>
2019-03-25 12:06:33 -0400

    Move most lein plugins into aliases
    ... so they aren't loaded for every lein incantation

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,6 +1,12 @@
 {:user {:plugins [[cider/cider-nrepl "0.21.1"]
                   [venantius/ultra "0.6.0"]]
-        :dependencies [[cljfmt "0.5.7"]
-                       [jonase/kibit "0.1.5"
-                        :exclusions [org.clojure/clojure]]
-                       [com.cemerick/pomegranate "1.0.0"]]}}
+        :dependencies [[com.cemerick/pomegranate "1.0.0"]]
+        ;; Instead of adding a bunch of plugins, use aliases for each one
+        :aliases {"kibit" ["with-profile" "+user-kibit" "kibit"]
+                  "pprint" ["with-profile" "+user-pprint" "pprint"]
+                  "cljfmt" ["with-profile" "+user-cljfmt" "cljfmt"]
+                  "ancient" ["with-profile" "+user-ancient" "ancient"]}}
+ :user-kibit {:plugins [[lein-kibit "0.1.6"]]}
+ :user-pprint {:plugins [[lein-pprint "1.2.0"]]}
+ :user-cljfmt {:plugins [[lein-cljfmt "0.6.4"]]}
+ :user-ancient {:plugins [[lein-ancient "0.6.15"]]}}
