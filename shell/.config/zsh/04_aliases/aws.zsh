1b57355798c7a2e791dd11cf73b20f7c36a97e1b
Mike Richards <mrichards42@gmx.com>
2023-07-12 14:44:18 -0400

    add awslocal alias

---
+++ shell/.config/zsh/04_aliases/aws.zsh
@@ -0,0 +1,1 @@
+alias awslocal="AWS_ACCESS_KEY_ID=fake AWS_SECRET_ACCESS_KEY=fake AWS_DEFAULT_REGION=${DEFAULT_REGION:-$AWS_DEFAULT_REGION} aws --endpoint-url=http://${LOCALSTACK_HOST:-localhost}:4566"
