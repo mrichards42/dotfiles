4aee482a3446f01128d9a5abcbba026acfcc7ea1
Mike Richards <mrichards42@gmx.com>
2025-01-08 12:27:13 -0500

    Auto-start tmux with fish inside regardless of the default shell

--- shell/.config/fish/conf.d/02-tmux.fish
+++ shell/.config/fish/conf.d/02-tmux.fish
@@ -1,6 +1,6 @@
 # https://github.com/budimanjojo/tmux.fish/blob/53846e9ae8530cc0d33b9337d3c333de6c00e170/conf.d/tmux.fish#L75C1-L75C48
 if status is-interactive && ! fish_is_root_user
   if type -q tmux && test -z $TMUX
-    tmux new -As0
+    tmux set-option -g default-shell $(command -v fish) \; new -As0
   end
 end
